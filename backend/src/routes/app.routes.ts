cc3bc98ac4888c3e74fef4efb82aedaf40e290a5
caioliveira <caio.oliveira277@etec.sp.gov.br>
2020-12-24 18:25:18 -0300

    configuracao do banco de dados, rotas e middlewares

--- backend/src/routes/app.routes.ts
+++ backend/src/routes/app.routes.ts
@@ -1,9 +1,13 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+
+import authMiddleware from '../app/middlewares/authMiddleware';
+
+import UserController from '../app/controllers/UserController';
+import AuthController from '../app/controllers/AuthController';
 
 const router = express.Router();
 
-router.get('/', (_req: Request, res: Response) => {
-  res.send('Home route');
-});
+router.post('/user', UserController.store);
+router.post('/auth', AuthController.authenticate);
 
 export default router;
--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -1,19 +1,15 @@
+import 'reflect-metadata';
+import '../database/connection';
 import express from 'express';
-import bodyParser from 'body-parser';
 import routes from "../routes/app.routes";
 import http from "http";
-import { Database } from "../database";
 
 const app = express();
 const server = http.createServer(app);
 
+app.use(express.json());
 app.use(routes);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 server.listen(process.env.PORT, async () => {
-  const conn = await new Database().connect();
-  
-  console.log(`Database is connected?: ${conn}`);
-  console.log(`Server is ready: PORT ${process.env.PORT}`);
+  console.log(`🔥 Server is ready: PORT ${process.env.PORT}`);
 });
