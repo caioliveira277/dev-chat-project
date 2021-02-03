c949fa7ba72f673de38e799e3d08720972a4de23
caioliveira <caio.oliveira277@etec.sp.gov.br>
2021-02-03 20:26:39 -0300

    adicionando redirecionamento de rotas noMatch

--- frontend/src/routes/public.routes.tsx
+++ frontend/src/routes/public.routes.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import {
   Route,
+  Redirect
 } from "react-router-dom";
 import * as Pages from 'pages';
 
 const PublicRoutes: React.FC = () => {
   return (
     <>
+      <Route path="*">
+        <Redirect to="/login" />
+      </Route>
       <Route exact path="/login" component={Pages.Login} />
       <Route exact path="/signin" component={Pages.Signin} />
       <Route exact path="/chat" component={Pages.Chat} />
