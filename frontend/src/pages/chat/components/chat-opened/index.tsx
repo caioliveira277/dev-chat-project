53db9817076aa700ced9d6554534da45751a4d10
caioliveira <caio.oliveira277@etec.sp.gov.br>
2021-02-06 19:42:25 -0300

    adicionando layout para chat nao selecionado

--- frontend/src/pages/chat/components/chat-opened/index.tsx
+++ frontend/src/pages/chat/components/chat-opened/index.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Container,
   Header,
   ContainerInfo,
   Image,
   Name,
-  LinkInfo
+  LinkInfo,
+  ContainerEmpty,
+  TextEmtpy
 } from './styles';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
 import TextArea from './input-message';
 import MessageList from './message-list';
+import { MdChat } from 'react-icons/md';
 
 const ChatOpened: React.FC = () => {
+  const [ selectedChat, setSelectedChat ] = useState(false);
   return (
     <Container>
-      <Header color='#3075C0'>
-        <ContainerInfo>
-          <Image src='http://localhost:3000/assets/images/group-profiles/typescript.png' alt='teste' />
-          <Name>Typescript</Name>
-        </ContainerInfo>
-        <LinkInfo href='#'>
-          <BsFillInfoCircleFill size='26px' />
-        </LinkInfo>
-      </Header>
-      <MessageList />
-      <TextArea />
+      {!selectedChat ? (
+        <ContainerEmpty>
+          <MdChat size='200px' color="#ACE7C9"/>
+          <TextEmtpy>
+            Selecione um grupo para inciar...
+          </TextEmtpy>
+        </ContainerEmpty>
+      ): (
+        <>
+          <Header color='#3075C0'>
+            <ContainerInfo>
+              <Image src='http://localhost:3000/assets/images/group-profiles/typescript.png' alt='teste' />
+              <Name>Typescript</Name>
+            </ContainerInfo>
+            <LinkInfo href='#'>
+              <BsFillInfoCircleFill size='26px' />
+            </LinkInfo>
+          </Header>
+          <MessageList />
+          <TextArea />
+        </>
+      )}
     </Container>
   );
 };
--- frontend/src/pages/chat/components/chat-opened/styles.ts
+++ frontend/src/pages/chat/components/chat-opened/styles.ts
@@ -47,3 +47,17 @@ export const LinkInfo = styled.a`
     color: ${({theme}) => darken(0.07, theme.colors.secondary)};
   }
 `;
+
+export const ContainerEmpty = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const TextEmtpy = styled.h2`
+  color: #BFC0BF;
+  font-size: 28px;
+  font-weight: 400;
+`;
