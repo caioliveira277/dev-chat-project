7fb2ba69e118a49832de2cb7e41e3dd8a5030b0f
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-06-02 20:06:51 -0300

    removendo rotas não utilizadas

--- backend/src/routes/app.routes.ts
+++ backend/src/routes/app.routes.ts
@@ -3,8 +3,6 @@ import AuthMiddleware from 'app/middlewares/authMiddleware';
 
 import AuthController from 'app/controllers/AuthController';
 import UserController from 'app/controllers/UserController';
-import GroupController from 'app/controllers/GroupController';
-import UserGroupController from 'app/controllers/UserGroupController';
 
 const router = express.Router();
 
@@ -15,16 +13,5 @@ router.post('/auth-token',[AuthMiddleware.authRoute,  AuthController.authenticat
 /* rotas dos usuários */
 router.post('/user', UserController.create);
 router.get('/user/:id', [AuthMiddleware.authRoute, UserController.find]);
-router.delete('/user', [AuthMiddleware.authRoute, UserController.delete]);
-
-/* rotas dos grupos */
-router.post('/group', [AuthMiddleware.authRoute, GroupController.create]);
-router.put('/group/:id', [AuthMiddleware.authRoute, GroupController.update]);
-router.get('/group/:id', [AuthMiddleware.authRoute, GroupController.find]);
-router.delete('/group/:id', [AuthMiddleware.authRoute, GroupController.delete]);
-
-/* rotas de relacinamento usuário e grupo */
-router.post('/user-group', [AuthMiddleware.authRoute, UserGroupController.create]);
-router.delete('/user-group', [AuthMiddleware.authRoute, UserGroupController.delete]);
 
 export default router;
