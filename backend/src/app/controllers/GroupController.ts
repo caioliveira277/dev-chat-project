86afbfefa1501a8d1d754a2131fccbb6eee44d87
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-06-20 11:45:08 -0300

    removendo importações não desnecessárias

--- backend/src/app/controllers/GroupController.ts
+++ backend/src/app/controllers/GroupController.ts
@@ -1,9 +1,4 @@
-import { Request, Response } from 'express';
-import { Group } from 'app/models/Group';
-import { Exception } from 'app/utilities';
-
 class GroupController {
-  
 }
 
 export default new GroupController();
