8dda0383aa2c307944e617c5b02c921af647b0aa
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-06-20 11:46:28 -0300

    removendo metodos não utilizados e adicionando um novo metodo para obter grupos disponíveis

--- backend/src/app/controllers/UserGroupController.ts
+++ backend/src/app/controllers/UserGroupController.ts
@@ -1,38 +1,11 @@
-import { Request, Response } from 'express';
-import { Group } from 'app/models/Group';
-import { User } from 'app/models/User';
 import { UserGroup } from 'app/models/UserGroup';
 import { Exception } from 'app/utilities';
 
-class GroupController {
-  public async create(req: Request, res: Response): Promise<any> {
-    try {
-      const { group_id, user_id } = req.body;
-
-      const group = await Group.findOne(group_id);
-      if (!group) throw new Exception('Groupo não encontrado', 400);
-
-      const user = await User.findOne(user_id);
-      if (!user) throw new Exception('Usuário não encontrado', 400);
-
-      const userGroupAssociationExists = await UserGroup.findOne({
-        where: {group_id, user_id}
-      });
-      if(userGroupAssociationExists) throw new Exception('Usuário já cadastrado', 400);
-      
-
-      const userGroupAssociation = new UserGroup();
-      userGroupAssociation.user = user;
-      userGroupAssociation.group = group;
-      await userGroupAssociation.save();
-
-      return res.json(userGroupAssociation.id);
-    } catch (error) {
-      return res.status(error.code).json({ message: error.message })
-    }
-  }
-
-  public async findUserGroups(userId: number): Promise<any> {
+interface IAvailableGroupIds {
+  [index: number]: number[]
+}
+class UserGroupController {
+  public async findUserGroups(userId: number): Promise<UserGroup[]> {
     try {
       const userGroups = await UserGroup.find({
         select: ['group_id'],
@@ -45,40 +18,7 @@ class GroupController {
       return userGroups;
     } catch (error) {
       Exception.interceptErrors(error);
-    }
-  }
-
-  public async findGroupUsers(req: Request, res: Response): Promise<any> {
-    try {
-      const { id } = req.params; 
-
-      const groupUsers = await UserGroup.find({
-        select: ['user_id'],
-        where: {
-          group_id: id
-        },
-        relations: ['user'],
-      });
-
-      return res.json(groupUsers);
-    } catch (error) {
-      return res.status(error.code).json({ message: error.message })
-    }
-  }
-
-  public async delete(req: Request, res: Response): Promise<any> {
-    try {
-      const { group_id, user_id } = req.body;
-
-      const userGroupAssociation = await UserGroup.findOne({ where: { group_id, user_id } });
-      if (!userGroupAssociation) throw new Exception('Este usuário não pertence ao grupo selecionado', 400);
-
-      const isDeleted = await userGroupAssociation.remove();
-      if (!isDeleted) throw new Exception('Falha ao remover usuário do grupo', 500);
-
-      return res.json({ message: 'Usuário removido do grupo com sucesso!' })
-    } catch (error) {
-      return res.status(error.code).json({ message: error.message })
+      return []
     }
   }
 
@@ -118,6 +58,32 @@ class GroupController {
       return false;
     }
   }
+
+  public async getAvailableGroupsAndUsers(): Promise<{groups: Omit<UserGroup, 'user'>[], participatingUserIds: IAvailableGroupIds}>{
+    try {
+      const groupsAndParticipantingUsers = await UserGroup.getGroupAndUsers();
+      const availablesGroupsId: IAvailableGroupIds = {};
+  
+      const formatedGroups = groupsAndParticipantingUsers.filter((userGroup) => {
+        if(!availablesGroupsId[userGroup.group_id]) {
+          availablesGroupsId[userGroup.group_id] = [userGroup.user_id];
+          return true
+        } else {
+          availablesGroupsId[userGroup.group_id].push(userGroup.user_id);
+          return false;
+        }
+      });
+
+      return {
+        groups: formatedGroups,
+        participatingUserIds: availablesGroupsId
+      }
+    } catch (error) {
+      Exception.interceptErrors(error);
+      return {groups: [], participatingUserIds: {}};
+    }
+  }
 }
 
-export default new GroupController();
+
+export default new UserGroupController();
